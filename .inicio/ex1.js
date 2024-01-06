d9412a549c6346656725cc5412de87b31aafa43c
NaderSouza <naderhadadsouza@gmail.com>
2024-01-06 01:39:45 -0300

    tomorrow

---
+++ .inicio/ex1.js
@@ -0,0 +1,32 @@
+console.log("Números Pares:");
+for (let i = 1; i <= 10; i++) {
+if (i % 2 === 0) {
+    console.log(i);
+}
+}
+
+console.log("\nNúmeros Ímpares:");
+for (let i = 1; i <= 10; i++) {
+if (i % 2 !== 0) {
+    console.log(i);
+}
+}
+
+{
+    console.log("\nNúmeros Pares 2:");
+console.log(2);
+console.log(4);
+console.log(6);
+console.log(8);
+console.log(10);
+}
+
+{
+    console.log("\nNúmeros Ímpares 2:");
+console.log(1);
+console.log(3);
+console.log(5);
+console.log(7);
+console.log(9);
+}
+
---
+++ .inicio/fonte.js
@@ -0,0 +1,11 @@
+{
+console.log(4 * 5 ,"oi");
+console.log(6 * 5 ,"oi");
+}
+
+{
+    console.log(4 * 5 ,"tchau");
+    console.log(6 * 5 ,"tchau");
+}
+
+console.log("final");
