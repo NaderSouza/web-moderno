321a901471dc7ad22bc59a93401484f58e9113b5
Nader Hadad Souza <naderhadadsouza@gmail.com>
2024-01-06 02:06:59 -0300

    tomorrow

---
+++ .inicio/dados2.js
@@ -0,0 +1,17 @@
+let preco = 20.10;
+let desconto = 0.4;
+
+console.log("\n1# " + 20.1 * 0.6);
+console.log("\n2# " +preco * (1 - desconto));
+
+let precoComDesconto = preco * (1 - desconto);
+console.log("\n3# " + precoComDesconto);
+
+let nome = "caderno" //String ---> sequência de símbolos
+let categoria = "papel";
+console.log("\nProduto: " + nome
+ + ", Categoria: " + categoria
+ + ", Preco: " + preco
+ + ", Desconto: " + desconto );
+
+ console.log(1 + 4);
