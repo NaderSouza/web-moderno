d5926d96fda1d45ab098f7625e1bacd54dfed6a8
NaderSouza <naderhadadsouza@gmail.com>
2024-01-08 05:19:12 -0300

    hj

---
+++ .inicio/desafio_area.js
@@ -0,0 +1,13 @@
+// PI * raio * raio
+
+let PI = 3.14;
+let raio = 10;
+let area = PI * raio * raio;
+
+// let area = "area";
+
+console.log("area é de " + area);
+
+area = Math.PI * raio * raio;
+
+console.log("area é de " + area);
---
+++ .inicio/desafio_troca.js
@@ -0,0 +1,15 @@
+let a = 7;
+let b = 94;
+
+console.log("Valores originais:");
+console.log("a:", a);
+console.log("b:", b);
+
+// Trocar os valores
+let temp = a;
+a = b;
+b = temp;
+
+console.log("\nValores trocados:");
+console.log("a:", a);
+console.log("b:", b);
