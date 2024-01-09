4cd1bf9cf841e4059d135534aaf91401a2d82892
Nader Hadad Souza <naderhadadsouza@gmail.com>
2024-01-09 16:57:49 -0300

    noite

---
+++ .fundamentos/array.js
@@ -0,0 +1,19 @@
+const valores = [7.7, 8.9, 1.8]
+console.log(valores[0], valores[3])
+console.log(valores[3])
+
+valores[3] = 10 
+console.log(valores)
+console.log(valores.length)
+
+valores.push({id: 3}, false, null, 'teste')
+console.log(valores)
+console.log(valores.length)
+
+
+console.log (valores. pop ())  
+delete valores [0]  
+console.log (valores)
+
+console.log (typeof valores)
+
---
+++ .fundamentos/objeto.js
@@ -0,0 +1,21 @@
+const prod1         = {}
+prod1.nome          = 'PolyStation 5'
+prod1.price         = 4760.90
+prod1['Desconto']   = 0.50
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'PC',
+    preco: 5080.75,
+    obj: {
+        blabla: 1,
+        obj: {
+            blabla: 2
+        }
+    }
+}
+
+console.log(prod2)
+
+'{ "nome": "iPhone", "preco": 4200,90}' // Formato JSON 
